1e346971fc6848b18943f799fec418f72102d9f2
anik-221 <khananik793@gmail.com>
2025-08-03 10:22:11 +0600

    fisrt comit

---
+++ src/diu/blood/bank/DiuBloodBank.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diu.blood.bank;
+
+/**
+ *
+ * @author DCL
+ */
+public class DiuBloodBank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
